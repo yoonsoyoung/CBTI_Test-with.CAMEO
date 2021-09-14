c51997957c7a8330a48938c5b185df512f74b5db
yoonsoyoung <32251962+yoonsoyoung@users.noreply.github.com>
2021-09-14 09:40:46 +0900

    Create AllCbtiList.js

---
+++ js/AllCbtiList.js
@@ -0,0 +1,19 @@
+const cbtis = [
+    "CHTS",
+    "CHTU",
+    "CHVS",
+    "CHVU",
+    "CITS",
+    "CITU",
+    "CIVS",
+    "CIVU",
+    "NHTS",
+    "NHTU",
+    "NHVS",
+    "NHVU",
+    "NITS",
+    "NITU",
+    "NIVS",
+    "NIVU"
+]
+export default cbtis
